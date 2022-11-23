b62ca7482e895188c055f1679a3272f32a9e7c4d
MisterAzix <flizi@live.fr>
2022-11-23 12:43:30 +0100

    feat(front): adding eslint airbnb & format code ✨

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -1,14 +1,10 @@
-import './globals.scss'
+import './globals.scss';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
