2f08602b347804bbd14e8173472fa96697875a67
Julian Laballe <julian.laballe@unlikely.technology>
2022-11-25 13:40:07 +0100

    feat(front): create redux store

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,18 +2,22 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import { LoginForm } from './components/LoginForm';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={'/'} element={<Home />} />
-        <Route path={'/login'} element={<LoginForm username={''} password={''} />} />
-        <Route path={'/register'} element={<h1>Register</h1>} />
-        <Route path={'/room'} />
-        <Route path="*" element={<h1>Erreur 404</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<Home />} />
+          <Route path={'/login'} element={<LoginForm username={''} password={''} />} />
+          <Route path={'/register'} element={<h1>Register</h1>} />
+          <Route path={'/room'} />
+          <Route path="*" element={<h1>Erreur 404</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
