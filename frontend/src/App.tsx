041bdfb619385caff09f7a1deaa203a979f9705b
Julian Laballe <julian.laballe@unlikely.technology>
2022-11-25 10:30:16 +0100

    feat(front): add routing (#2)

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,39 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={'/'}
+          element={
+            <div className="App">
+              <h1>Home</h1>
+            </div>
+          }
+        />
+        <Route
+          path={'/login'}
+          element={
+            <div className="App">
+              <h1>Login</h1>
+            </div>
+          }
+        />
+        <Route
+          path={'/register'}
+          element={
+            <div className="App">
+              <h1>Register</h1>
+            </div>
+          }
+        />
+        <Route path="*" element={<h1>Erreur 404</h1>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
