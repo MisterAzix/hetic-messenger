aecaa19d3d22f750de03f39927bf9f8d56473e86
Julian Laballe <julian.laballe@unlikely.technology>
2022-12-10 16:48:02 +0100

    feat(front): add RegisterForm
    Co-authored-by: aniscoquelet <48022945+aniscoquelet@users.noreply.github.com>
    Co-authored-by: Maxence Breuilles <40914400+MisterAzix@users.noreply.github.com>

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import { Button } from "@react-native-material/core";
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
@@ -14,7 +13,6 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Button title="Click Me" onPress={() => alert("🎉🎉🎉")}/>
         <Navigation colorScheme={colorScheme} />
         <StatusBar />
       </SafeAreaProvider>
--- mobile/screens/TabOneScreen.tsx
+++ mobile/screens/TabOneScreen.tsx
@@ -1,15 +1,15 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import {  View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
+import RegisterForm from '../features/Register/RegisterForm'
+import {Text} from "@react-native-material/core"
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Text style={styles.heading} variant='h2'>Register now!</Text>
+      <RegisterForm/>
     </View>
   );
 }
@@ -17,16 +17,10 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20,
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
+  heading: {
+    marginBottom: 25,
+  }
 });
